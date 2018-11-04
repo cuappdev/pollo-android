87c2a45a69bbe796cb3b34cb279df8e898e480a2
Jehron Petty <jwp263@cornell.edu>
2018-11-04 14:57:04 -0500

    Initial socket setup

--- app/src/main/java/com/cornellappdev/android/pollo/GroupHomeActivity.java
+++ app/src/main/java/com/cornellappdev/android/pollo/GroupHomeActivity.java
@@ -2,12 +2,24 @@ package com.cornellappdev.android.pollo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import java.net.URISyntaxException;
 
 public class GroupHomeActivity extends AppCompatActivity {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket(getString(R.string.deployed_backend));
+        } catch (URISyntaxException e) {}
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_home);
+        mSocket.connect();
     }
 }
