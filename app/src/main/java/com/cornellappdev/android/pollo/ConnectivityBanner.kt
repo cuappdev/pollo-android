2153bb468f7497a80021bfb686deb70dfda3e0bf
Shiyuan-Huang-23 <shiyuancn@live.cn>
2020-11-03 13:18:37 -0500

    Persist banners when disconnected

--- app/src/main/java/com/cornellappdev/android/pollo/ConnectivityBanner.kt
+++ app/src/main/java/com/cornellappdev/android/pollo/ConnectivityBanner.kt
@@ -1,6 +1,7 @@
 package com.cornellappdev.android.pollo
 
 import android.content.Context
+import android.graphics.Color
 import android.net.ConnectivityManager
 import android.net.Network
 import android.net.NetworkRequest
@@ -11,6 +12,7 @@ import com.google.android.material.snackbar.Snackbar
 object ConnectivityBanner {
     private var currContext: Context? = null;
     private var currView: View? = null
+    private var isConnected = true
 
     // Registers a new NetworkCallback if one does not currently exist or updates existing network callback
     fun setUpConnectivityBanners(context: Context, view: View) {
@@ -22,22 +24,32 @@ object ConnectivityBanner {
             val callback = object : ConnectivityManager.NetworkCallback() {
                 override fun onAvailable(network: Network) {
                     super.onAvailable(network)
-                    val polloGreen = ContextCompat.getColor(currContext!!, R.color.polloGreen)
-                    val snackbar = Snackbar.make(currView!!, "Connected", Snackbar.LENGTH_SHORT).setBackgroundTint(polloGreen)
-                    snackbar.show()
+                    isConnected = true
+                    displaySnackbar()
                 }
 
                 override fun onLost(network: Network) {
                     super.onLost(network)
-                    val errorRed = ContextCompat.getColor(currContext!!, R.color.red)
-                    val snackbar = Snackbar.make(currView!!, "Not Connected", Snackbar.LENGTH_LONG).setBackgroundTint(errorRed)
-                    snackbar.show()
+                    isConnected = false
+                    displaySnackbar()
                 }
             }
             cm.registerNetworkCallback(builder.build(), callback)
         } else {
             currContext = context
             currView = view
+            if (!isConnected) {
+                displaySnackbar()
+            }
         }
+    }
+    
+    // Displays appropriate connectivity banner based on current connectivity status
+    private fun displaySnackbar() {
+        val color = ContextCompat.getColor(currContext!!, if (isConnected) R.color.polloGreen else R.color.red)
+        val text = if (isConnected) "Connected" else "Not Connected"
+        val duration = if (isConnected) Snackbar.LENGTH_SHORT else Snackbar.LENGTH_INDEFINITE
+        val snackbar = Snackbar.make(currView!!, text, duration).setBackgroundTint(color)
+        snackbar.show()
     }
 }
